cfcb15994a5bd0742ae89fd2860ae43aef6dd67d
mxr <1977368805@qq.com>
2025-02-27 17:52:29 +0800

    feat: mxr

---
+++ src/learning/mxr/Main.java
@@ -0,0 +1,19 @@
+package learning.mxr;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //List<String>lines = new ArrayList<String>();
+        Set<String>lines = new HashSet<>()
+        lines.add("Hello");
+        lines.add("world");
+        lines.add("!");
+        lines.add(index 1,element "java");
+        for(String line:lines) {
+            System.out.print(line);
+    }
+}
