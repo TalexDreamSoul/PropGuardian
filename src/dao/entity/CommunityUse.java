4e76a0547680af5655321f64e6168335216c8cb9
TalexDreamSoul <59305952+talexdreamsoul@users.noreply.github.com>
2025-03-06 15:45:54 +0800

    feat(dao): 添加小区用水用电用气数据实体类
    - 新增 CommunityUse 类，用于表示小区每日用水用电用气数据
    - 类包含小区ID、日期、用水量、用电量、用气量等属性
    - 提供了构造函数、getter/setter 方法以及 toString 方法
    - 删除了 Main 类中的调试代码

---
+++ src/dao/entity/CommunityUse.java
@@ -0,0 +1,76 @@
+package dao.entity;
+
+public class CommunityUse {
+    // 小区ID
+    private long districtId;
+    // 日期
+    private int dateInt11;
+    // 用水量
+    private double totWaterReading;
+    // 用电量
+    private double totElecReading;
+    // 用气量
+    private double secSupplyReading;
+
+    // 构造函数
+    public CommunityUse(long districtId, int dateInt11, double totWaterReading, double totElecReading, double secSupplyReading) {
+        this.districtId = districtId;
+        this.dateInt11 = dateInt11;
+        this.totWaterReading = totWaterReading;
+        this.totElecReading = totElecReading;
+        this.secSupplyReading = secSupplyReading;
+    }
+
+    // Getter 和 Setter 方法
+    public long getDistrictId() {
+        return districtId;
+    }
+
+    public void setDistrictId(long districtId) {
+        this.districtId = districtId;
+    }
+
+    public int getDateInt11() {
+        return dateInt11;
+    }
+
+    public void setDateInt11(int dateInt11) {
+        this.dateInt11 = dateInt11;
+    }
+
+    public double getTotWaterReading() {
+        return totWaterReading;
+    }
+
+    public void setTotWaterReading(double totWaterReading) {
+        this.totWaterReading = totWaterReading;
+    }
+
+    public double getTotElecReading() {
+        return totElecReading;
+    }
+
+    public void setTotElecReading(double totElecReading) {
+        this.totElecReading = totElecReading;
+    }
+
+    public double getSecSupplyReading() {
+        return secSupplyReading;
+    }
+
+    public void setSecSupplyReading(double secSupplyReading) {
+        this.secSupplyReading = secSupplyReading;
+    }
+
+    // toString 方法，用于打印对象信息
+    @Override
+    public String toString() {
+        return "CommunityUse{" +
+                "districtId=" + districtId +
+                ", dateInt11=" + dateInt11 +
+                ", totWaterReading=" + totWaterReading +
+                ", totElecReading=" + totElecReading +
+                ", secSupplyReading=" + secSupplyReading +
+                '}';
+    }
+}
