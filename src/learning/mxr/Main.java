c9266f858f2d9bfadffb3915a11a6151109d9211
mxr <1977368805@qq.com>
2025-03-18 21:22:52 +0800

    refactor(learning): 重构学习代码
    - 新增 Main.java 文件，实现了一个简单的列表操作示例
    - 清空 Step1Main.java 和 Step2Main.java 文件的内容
    - 为 Step2Main.java 添加空行，准备后续编写代码

--- src/learning/mxr/Main.java
+++ src/learning/mxr/Main.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
-public class Main {
+public class       Main {
     public static void main(String[] args) {
 
         //List<String>lines = new ArrayList<String>();
