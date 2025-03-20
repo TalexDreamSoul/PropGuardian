246b8f2aad2107f858bb8682ef75223889f63a0c
20160 <2016047057@qq.com>
2025-03-20 17:08:44 +0800

    feat(db): 为 MySql 类添加连接和断开连接方法
    - 在 MySql 类中添加了 connect 和 disconnect 方法
    - 添加了 Env 类的导入，用于获取 MySQL 连接 URL
    - 在构造函数中初始化了 MySQL 连接 URL

--- src/db/MySql.java
+++ src/db/MySql.java
@@ -1,5 +1,18 @@
 package db;
 
-public class MySql {
+import configuration.Env;
 
+public class MySql {
+    public MySql() {
+        Env instance = Env.getInstance();
+        String url = instance.getProperty("mysql");
+    }
+
+    public void connect() {
+
+    }
+
+    public void disconnect() {
+
+    }
 }
