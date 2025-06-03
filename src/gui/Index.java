1c8cd87ea8b82b1aa17a1b4ac6262d4bcfd555ea
李学琳 <2905626020@qq.com>
2025-06-03 22:18:53 +0800

    feat(gui): 添加物业收费报表功能
    - 新增 PropertyBillingReport 类实现物业收费报表功能
    - 在 Index 类中添加物业收费报表菜单项
    - 更新项目依赖库版本

--- src/gui/Index.java
+++ src/gui/Index.java
@@ -106,7 +106,9 @@ public class Index extends JFrame {
             UserBillingReport userBillingReport = new UserBillingReport();
             userBillingReport.setVisible(true);
         });
-
+        propertyBillItem.addActionListener(e -> {
+            new PropertyBillingReport().setVisible(true);
+        });
         // 添加 Exit menu item 到菜单栏
         JMenuItem exitItem = new JMenuItem("Exit");
         exitItem.addActionListener(e -> System.exit(0));
