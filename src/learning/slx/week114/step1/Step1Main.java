2caeb32ac5799af4cdf3e190ac9c008f32f86bdc
lzysl <3535693649@qq.com>
2025-06-05 22:37:57 +0800

    feat(gui): 添加用户信息管理功能
    - 在菜单栏中新增用户信息管理菜单项
    - 实现用户信息查询、添加、修改和删除功能
    - 新增 UserInfoPage 类用于用户信息管理界面
    - 更新 Index 类，添加用户信息管理相关代码

---
+++ src/learning/slx/week114/step1/Step1Main.java
@@ -0,0 +1,16 @@
+package learning.slx.week114.step1;
+
+public class Step1Main {
+
+    public static void main(String[] args) {
+        Penguin penguin = new Penguin("Penguin1");
+        Mouse mouse = new Mouse("Mouse1");
+
+        penguin.introduceSelf();
+        penguin.sleep();
+
+        mouse.introduceSelf();
+        mouse.eat();
+    }
+
+}
