7e319db5397c3c930554ec6d10f5847e7546c466
zhy <2408407376@qq.com>
2025-06-17 21:42:24 +0800

    feat(learning): 添加多个独立的 Java 项目
    - 新增多个 Java 项目，每个项目包含 Animal、Mouse、Penguin 类和主程序
    - 每个项目位于不同的包路径下，如 hhhhhh、oooo、ppppp等
    - 新增 StudentExtractor 类，用于从数据库中提取学生信息
    - 删除原有的 Main.java 文件

---
+++ src/learning/zhy/zzzzz/step1/Penguin.java
@@ -0,0 +1,11 @@
+package learning.zhy.zzzzz.step1;
+
+public class Penguin extends Animal {
+    public Penguin(String name) {
+        super("PENGUIN", name);
+    }
+
+    public void sleep() {
+        System.out.println("Hello, sleeping now.");
+    }
+}
