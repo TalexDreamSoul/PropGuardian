39714aa0d399de0415ea92d4f809f9f2e7d5d85e
mxr <1977368805@qq.com>
2025-06-18 14:31:26 +0800

    feat(learning): 添加新的 Java 类和方法
    - 新增 Animal、Mouse、Penguin 类以及 Step1Main 主程序
    - 实现了动物自我介绍、老鼠吃东西、企鹅睡觉等功能
    - 添加了从数据库中提取学生信息的功能- 更新了项目依赖库版本

---
+++ src/learning/mxr/week121/step4/week111/step1/Animal.java
@@ -0,0 +1,17 @@
+package learning.mxr.week121.step4.week111.step1;
+
+import lombok.Data;
+
+@Data
+public class Animal {
+    private final String id, name;
+
+    public Animal(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public void introduceSelf() {
+        System.out.println("Hello, I'm " + this.name);
+    }
+}
